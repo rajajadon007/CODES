7a405984f1e2e9f2353b54a769848d3d22ed16ab
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 11:35:17 +0530

    Update NEWS_SCRAPPER.py

--- NEWS_SCRAPPER.py
+++ NEWS_SCRAPPER.py
@@ -3,15 +3,15 @@ import json
 import requests
 from newsapi import NewsApiClient
 from pandas.io.json import json_normalize
-# Create an instance of the NewsApiClient class
+
 newsapi = NewsApiClient(api_key='d5f016a9f9be4a6d817211ca9833172b')
 
 def top_headlines():
-    # Get the country and category from the user
+   
     country = st.selectbox("Which country are you interested in?", ["in", "us", "gb", "de", "fr"])
     category = st.selectbox("Which category are you interested in?", ["business", "entertainment", "general", "health", "science", "technology"])
 
-    # Get the top headlines for the specified country and category
+   
     top_headlines = newsapi.get_top_headlines(
         category=category, language='en', country=country,page=3)
     top_headlines = json_normalize(top_headlines['articles'])
