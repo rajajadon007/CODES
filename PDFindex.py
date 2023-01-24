9ff3ea92a593ea13fa527818c56ffdf3767770ac
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 18:49:42 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -2,17 +2,25 @@ import streamlit as st
 import pdfplumber
 import pandas as pd
 
-
+\
 with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
     text = ""
-    
+   
     for page in pdf.pages:
         text += page.extract_text()
-    # Write the extracted text to a CSV file
-    with open('your_pdf_text.csv', 'w') as f:
-        f.write(text)
-file_upload = st.file_uploader("Upload your CSV file", type=["csv"])
 
-if file_upload is not None:
-    df = pd.read_csv(file_upload)
-    st.dataframe(df)
+
+df = pd.DataFrame({'text':text.split("\n")})
+
+csv = df.to_csv(index=False)
+
+
+st.write("Here is the extracted text from the pdf:")
+st.dataframe(df)
+st.markdown("You can download the csv file by clicking the following button:")
+download_button = st.button('Download CSV')
+if download_button:
+    st.write("The extracted text is downloaded as csv file!")
+    st.markdown("Please find the downloaded csv file.")
+    st.write("You can also see the text below:")
+    st.text
