4f49b2f7f85690db6556e651b1cb7c28132bd1f2
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 18:21:21 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -6,9 +6,9 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
     for page in pdf.pages:
           df_list.append(page.extract_table())
         if table:   
-            df = pd.DataFrame(table[1:], columns=table[0])
-            df.index+=1
-            df_list.append(df)
+          df = pd.DataFrame(table[1:], columns=table[0])
+          df.index+=1
+          df_list.append(df)
    
     final_df = pd.concat(df_list, ignore_index=True)
    
