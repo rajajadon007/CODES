f19789b29d76d5899859014c91e079969c345f7c
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 17:47:38 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pdfplumber
 import pandas as pd
-with pdfplumber.open('y.pdf') as pdf:
+with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
     df_list = []
     for page in pdf.pages:
         df_list.append(page.extract_table())
