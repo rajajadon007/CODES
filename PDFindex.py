6e07d22c8d3f60de9aa53ceae5e9b889a6cd71b8
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 17:51:53 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -4,7 +4,7 @@ import pandas as pd
 with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
     df_list = []
     for page in pdf.pages:
-       df_list.append(page.extract_table())
+           df_list.append(page.extract_table())
        df = pd.concat(df_list, ignore_index=True)
        df.index+=1
        st.dataframe(df)
