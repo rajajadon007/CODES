254e6d0128b46b7025391ae2c7d8e5eab5a13fa2
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 16:54:19 +0530

    Update SOP.py

--- SOP.py
+++ SOP.py
@@ -1,7 +1,12 @@
-import streamlit as st
 import json
-
-with open('export.json') as f:
-    data = json.load(f)
-    
-st.write(data)
+import PyPDF2
+with open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+   
+    text = ''
+    for page in range(pdf_reader.numPages):
+        text += pdf_reader.getPage(page).extractText()
+ 
+    data = {'text': text}
+    with open('your_json_file.json', 'w') as json_file:
+        json.dump(data, json_file)
