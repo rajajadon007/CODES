d73dee47476176b646811b61b37e10844d0fc85b
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 19:19:55 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -12,7 +12,7 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
 
 df = pd.DataFrame({'text':text.split("\n")})
 st.dataframe(df,width=800)
-df_cols = df[["text":"CONTENT"]]
+df_cols = df[["text"]]
 df_json = df_cols.to_json(orient='index')
 st.write(df_json)
 
