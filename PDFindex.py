8147d0fdef4b9950948ceb0c068b06814cfc6d29
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 19:01:02 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -10,16 +10,12 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
         text += page.extract_text()
 
 
-df = pd.DataFrame({'text':text.split("\n")})
-
-
+df = pd.DataFrame({'text':text.split("\n")}))
 df.to_csv('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.csv', index=False)
-
-
 st.write("Here is the extracted text from the pdf:")
-st.dataframe(df)
+st.dataframe(df,width=800)
 st.markdown("You can download the csv file by clicking the following button:")
-download_button = st.button('Download CSV')
+download_button = st.button('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.csv')
 if download_button:
     st.write("The extracted text is downloaded as csv file!")
     st.markdown("Please find the downloaded csv file.")
