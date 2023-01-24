67606a84ab3ab3eb4cc701da592a54542c791706
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 19:07:59 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -11,11 +11,7 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
 
 
 df = pd.DataFrame({'text':text.split("\n")})
-df.to_csv('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.csv', index=False)
-st.write("Here is the extracted text from the pdf:")
 st.dataframe(df,width=800)
-st.markdown("You can download the csv file by clicking the following button:")
-download_button = st.download('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.csv')
-if download_button:
-    st.write("The extracted text is downloaded as csv file!")
-    st.markdown("Please find the downloaded csv file.")
+df_json = df.to_json()
+st.write(df_json)
+
