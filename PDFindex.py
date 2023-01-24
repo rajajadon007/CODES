581f1677b3a878a6fe7948254215cb978f0863c8
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-24 18:42:05 +0530

    Update PDFindex.py

--- PDFindex.py
+++ PDFindex.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pdfplumber
 import pandas as pd
-with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
-    table_list = []
+
+
+with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf.pdf') as pdf:
+    text = ""
+    
     for page in pdf.pages:
-        table = page.extract_table()
-        if table:
-            table_list.append(table)
-   
-    final_df = pd.concat(table_list, ignore_index=True,join='outer')
-   
-    st.dataframe(final_df)
+        text += page.extract_text()
+    # Write the extracted text to a CSV file
+    with open('your_pdf_text.csv', 'w') as f:
+        f.write(text)
+file_upload = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if file_upload is not None:
+    df = pd.read_csv(file_upload)
+    st.dataframe(df)
