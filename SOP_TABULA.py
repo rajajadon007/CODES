e163a3c976d1ecd0f8ca58c81573428fd0b1437f
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-25 18:46:59 +0530

    Update SOP_TABULA.py

--- SOP_TABULA.py
+++ SOP_TABULA.py
@@ -3,7 +3,7 @@ import tabula
 import json
 
 
-df = tabula.read_pdf("example.pdf", pages="all")
+df = tabula.read_pdf("SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf", pages="2")
 
 
 json_data = df.to_json(orient='records')
