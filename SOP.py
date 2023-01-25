2738885b01a5746f0b4a947a9e79308ff8e50397
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-25 16:27:31 +0530

    Update SOP.py

--- SOP.py
+++ SOP.py
@@ -4,6 +4,8 @@ import pdfplumber
 from pandas.io.json import json_normalize
 import pandas as pd
 
+import streamlit as st
+
 with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
     pages = []
     for i, page in enumerate(pdf.pages):
@@ -12,5 +14,14 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
     df = pd.DataFrame(pages)
     df.set_index('page_number', inplace=True)
     st.write(df, height =1000,width=1000)
+
+search_term = st.text_input("Enter a value from column 2:")
+if search_term:
+    result = df.query("column_2 == @search_term")
+    if result.empty:
+        st.warning("No match found.")
+    else:
+        st.write("The corresponding value in column 6 is:", result['column_6'].values[0])
+
 
 
