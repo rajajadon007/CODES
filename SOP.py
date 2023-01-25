7425f0640993d2b14bc14cb7008377096c50a812
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-25 17:08:43 +0530

    Update SOP.py

--- SOP.py
+++ SOP.py
@@ -3,9 +3,6 @@ import json
 import pdfplumber
 from pandas.io.json import json_normalize
 import pandas as pd
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter, landscape
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
     pages = []
@@ -13,14 +10,14 @@ with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as 
         text = page.extract_text()
         pages.append({'page_number': i+1, 'text': text})
     df = pd.DataFrame(pages)
-    if 'page_number' not in df.columns:
-        df['page_number'] = df.index
-    if df.index.name != 'page_number':
-        df.set_index('page_number', inplace=True)
+    df.set_index('page_number', inplace=True)
     st.write(df, height =1000,width=1000)
     search_term = st.text_input("Enter a search term:")
     if search_term:
         search_result = df[df['text'].str.contains(search_term, case=False)]
         st.write("Results for search term '{}':".format(search_term))
-        st.write(search_result, height =1000,width=1000)
-       
+        st.write(search_result.to_string(), unsafe_allow_html=True)
+        if st.button("Get Specific Result"):
+            specific_result = search_result[search_result['text'].str.contains(search_term, case=False)]
+            st.write("Specific Result for search term '{}':".format(search_term))
+            st.write(specific_result.to_string(), unsafe_allow_html=True)
