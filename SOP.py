aeedc7abc8b07fc3774de6daad1370d892ad1d5b
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-25 16:18:28 +0530

    Update SOP.py

--- SOP.py
+++ SOP.py
@@ -4,15 +4,12 @@ import pdfplumber
 from pandas.io.json import json_normalize
 
 with pdfplumber.open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf') as pdf:
-    text = ""
     pages = []
     for i, page in enumerate(pdf.pages):
-        text += page.extract_text()
+        text = page.extract_text()
         pages.append({'page_number': i+1, 'text': text})
-    df = json_normalize(pages)
+    df = pd.DataFrame(pages)
     df.set_index('page_number', inplace=True)
     st.write(df, height =1000,width=1000)
 
-
-       
 
