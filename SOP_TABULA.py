4bb5e966a9731a9340627a8eb84407dbf2578107
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-25 19:01:16 +0530

    Update SOP_TABULA.py

--- SOP_TABULA.py
+++ SOP_TABULA.py
@@ -2,11 +2,6 @@ import streamlit
 import tabula
 import json
 
-
 df = tabula.read_pdf("SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf", pages="2")
-
-
 json_data = df.to_json(orient='records')
-
-
 st.write(json_data)
