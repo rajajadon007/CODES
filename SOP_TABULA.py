7257e0d607fdda94a858e6b4e9c3900f1853af64
Raja Jadon <91981866+rajajadon007@users.noreply.github.com>
2023-01-27 13:07:09 +0530

    Update SOP_TABULA.py

--- SOP_TABULA.py
+++ SOP_TABULA.py
@@ -1,18 +1,10 @@
-from docx import Document
-import pandas as pd
+import tabula
 import streamlit as st
+import json
+
+df = tabula.read_pdf(
+    "SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf", stream=False, pages='all')
+st.dataframe(df)
 
 
-document = Document('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.docx')
-for table in document.tables:
-    data = []
-    keys = None
-    for i, row in enumerate(table.rows):
-        text = (cell.text for cell in row.cells)
-        if i == 0:
-            keys = tuple(text)
-            continue
-        row_data = dict(zip(keys, text))
-        data.append(row_data)
-    df = pd.DataFrame(data)
-    st.write(df, height=1000, width=1000)
+st.write(df)
